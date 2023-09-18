541d1cb1d34dcb3846c06aaf7f83ca23e952fd98
sunlight9344 <sunlight9344@gmail.com>
2023-09-18 10:51:43 +0900

    update

--- mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.poscodx.mysite.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.poscodx.mysite.service.UserService;
 import com.poscodx.mysite.vo.UserVo;
@@ -21,7 +25,7 @@ public class UserController {
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
 	public String join(UserVo vo) {
-		userService.addUser(vo);
+		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 	
@@ -33,5 +37,44 @@ public class UserController {
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public String login() {
 		return "user/login";
+	}
+	
+	@RequestMapping(value="/auth", method=RequestMethod.POST)
+	public String auth(
+		HttpSession session,
+		@RequestParam(value="email", required=true, defaultValue="") String email,
+		@RequestParam(value="password", required=true, defaultValue="") String password,
+		Model model){
+		
+		UserVo authUser = userService.getUser(email, password);
+		
+		if(authUser == null) {
+			model.addAttribute("email", email);
+			return "user/login";
+		}
+		
+		/*인증 처리*/
+		session.setAttribute("authUser", authUser);
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("authUser");
+		session.invalidate();
+		
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.GET)
+	public String update(HttpSession session) {
+		// Access Control(update page 접근제어)
+		UserVo authUser = (UserVo) session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/user/login";
+		}
+		
+		return "user/update";
 	}
 }
