8322dd998a8f099b104c8e585c4926b4ae70f488
sunlight9344 <sunlight9344@gmail.com>
2023-09-21 12:16:33 +0900

    mysite logback

--- mysite03/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.poscodx.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler
 	public String handlerException(Model model, Exception e) {
 		//1. 로깅(Logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors.toString());
+		logger.error(errors.toString());
 		
 		//2. apple 페이지
 		model.addAttribute("errors", errors.toString());
