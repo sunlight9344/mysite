9e87b16fb7485baaa404313c2d51ec738740e6eb
sunlight9344 <sunlight9344@gmail.com>
2023-10-06 10:51:32 +0900

    mysite04 - web config #2

--- mysite04/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite04/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -66,6 +66,14 @@ public class UserController {
 		authUser.setName(userVo.getName());
 		return "redirect:/user/update";
 	}
+	
+	@RequestMapping("/auth")
+	public void auth() {
+	}
+	
+	@RequestMapping("/logout")
+	public void logout() {
+	}
 
 //	@ExceptionHandler(Exception.class)
 //	public String handlerException() {
